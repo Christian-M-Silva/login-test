fcc162b078736cc08cb2d6c8a34bd0e4333a1c40
Christian Silva <tininjacms@gmail.com>
2025-05-15 15:04:28 -0300

    feat(criarApi): Criará estrutura inicial da API

---
+++ Back/LoginTest/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoginTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // POST api/<LoginController>
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            return Ok(value);
+        }
+    }
+}
