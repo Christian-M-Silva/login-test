3af0be9175df71533ddeb1f35e80406b13d48c64
Christian Silva <tininjacms@gmail.com>
2025-07-23 15:49:33 -0300

    feat(cypres): Instalará e configurará o cypress

---
+++ Front/cypress/e2e/login.cy.ts
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+  }) //Parei na parte de ver como conseguir os types do cypress para o ts para de gritar e começar a escreer os testes
+})
