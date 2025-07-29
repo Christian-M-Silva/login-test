9ae0c6be30418f7f5e8103fcd719b23183a86669
Christian-M-Silva <tininjacms@gmail.com>
2025-07-29 15:23:51 -0300

    test(cypresLogin): Login falhando com intercept

--- Front/cypress/e2e/login.cy.ts
+++ Front/cypress/e2e/login.cy.ts
@@ -1,13 +1,12 @@
 describe("Login", () => {
   beforeEach(() => {
     cy.visit("/");
-
-    cy.intercept("POST", "https://localhost:44346/api/Login", {
-      fixture: "response-login-sucess.json",
-    }).as("loginRequest");
   });
 
   it('should sucess login with "admin@admin.com" e "123456"', () => {
+    cy.intercept("POST", "https://localhost:44346/api/Login", {
+      fixture: "response-login-sucess.json",
+    }).as("loginRequestSuccess");
     const inputEmail = cy.get('[data-cy="input-email"]');
     const inputPassword = cy.get('[data-cy="input-password"]');
     const buttonLogin = cy.get('[data-cy="button-login"]');
@@ -16,8 +15,23 @@ describe("Login", () => {
 
     buttonLogin.click();
 
-    cy.wait("@loginRequest");
+    cy.wait("@loginRequestSuccess");
 
-    cy.url().should('include', '/dashboard')
-  }); 
+    cy.url().should("include", "/dashboard");
+  });
+
+  it("should failed login", () => {
+    cy.intercept("POST", "https://localhost:44346/api/Login", {
+      statusCode: 401,
+      body: { message: "Usuário ou senha inválidos" },
+    }).as("loginRequestFailed");
+    cy.get('[data-cy="input-email"]').type("user@admin.com");
+    cy.get('[data-cy="input-password"]').type("1234");
+    cy.get('[data-cy="button-login"]').click();
+    cy.wait("@loginRequestFailed");
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Usuário ou senha inválidos');
+    });
+  });
 });
